8c1f33a4e80572d78017e147726e06aa93c474a2
Edwin Martinez <edwin.martinez04@uptc.edu.co>
2026-05-30 12:27:07 -0500

    Initial commit

---
+++ src/main/java/com/dian/soap/DianSoap/DianSoapApplication.java
@@ -0,0 +1,13 @@
+package com.dian.soap.DianSoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DianSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DianSoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dian/soap/DianSoap/DianSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dian.soap.DianSoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DianSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
