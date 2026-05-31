92fe9e9a09073fa0db50e2c1248a8af2181708e5
AlissonPaez <alisson.paez@uptc.edu.co>
2026-05-30 22:08:02 -0500

    Confirmacion del correo

--- src/main/java/com/dian/soap/DianSoap/controller/EmailService.java
+++ src/main/java/com/dian/soap/DianSoap/controller/EmailService.java
@@ -1,8 +1,5 @@
 package com.dian.soap.DianSoap.controller;
 
-import com.dian.soap.DianSoap.entities.InvoiceRequest;
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +7,11 @@ import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import com.dian.soap.DianSoap.entities.InvoiceRequest;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
@@ -31,14 +33,32 @@ public class EmailService {
             return;
         }
 
-        MimeMessage message = mailSender.createMimeMessage();
         try {
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setTo(request.getCorreoCliente());
-            helper.setSubject("Confirmacion de factura " + invoiceNumber);
-            helper.setText("Su factura electronica fue generada exitosamente.");
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setTo(request.getCorreoVendedor());
+            helper.setSubject("Confirmación de envío - Factura " + invoiceNumber);
+            helper.setText(
+                "<h2>Hola " + request.getNombreVendedor() + ",</h2>" +
+                "<p>La factura electrónica fue generada y enviada exitosamente.</p>" +
+                "<ul>" +
+                "<li><b>Número de factura:</b> " + invoiceNumber + "</li>" +
+                "<li><b>Cliente:</b> " + request.getNombreCliente() + "</li>" +
+                "<li><b>Identificación cliente:</b> " + request.getIdentificacionCliente() + "</li>" +
+                "<li><b>Producto:</b> " + request.getProductoVendido() + "</li>" +
+                "<li><b>Cantidad:</b> " + request.getCantidadVendida() + "</li>" +
+                "<li><b>Precio unitario:</b> $" + request.getPrecioUnitario() + "</li>" +
+                "</ul>" +
+                "<p>Encuentra la factura adjunta en este correo.</p>" +
+                "<br><small>Este es un correo automático, por favor no responder.</small>",
+                true
+            );
+
             helper.addAttachment("factura-" + invoiceNumber + ".pdf", new ByteArrayResource(pdfBytes));
             mailSender.send(message);
+            logger.info("Correo enviado al vendedor: {}", request.getCorreoVendedor());
+
         } catch (MessagingException ex) {
             logger.error("Error enviando correo para la factura {}", invoiceNumber, ex);
         }
